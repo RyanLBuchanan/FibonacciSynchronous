8360aba94a0e2b05762d3cf768990058cbfe1db0
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-10-28 10:51:10 -0600

    Add project files.

---
+++ SynchronousTestForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FibonacciSynchronous
+{
+    partial class SynchronousTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.seqFiblabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(24, 40);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(368, 35);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start Sequential Fibonacci Calls";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(24, 88);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(368, 256);
+            this.outputTextBox.TabIndex = 1;
+            // 
+            // seqFiblabel
+            // 
+            this.seqFiblabel.AutoSize = true;
+            this.seqFiblabel.Location = new System.Drawing.Point(24, 8);
+            this.seqFiblabel.Name = "seqFiblabel";
+            this.seqFiblabel.Size = new System.Drawing.Size(369, 20);
+            this.seqFiblabel.TabIndex = 2;
+            this.seqFiblabel.Text = "Sequential calls to Fibonacci(46) and Fibonacci(45)";
+            // 
+            // SynchronousTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 360);
+            this.Controls.Add(this.seqFiblabel);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.startButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "SynchronousTestForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Synchronous Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label seqFiblabel;
+    }
+}
+
---
+++ SynchronousTestForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FibonacciSynchronous
+{
+    public partial class SynchronousTestForm : Form
+    {
+        public SynchronousTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = "Calculating Fibonacci(46)\r\n";
+            outputTextBox.Refresh();
+            DateTime startTime1 = DateTime.Now;
+            long result1 = Fibonacci(46);
+            DateTime endTime1 = DateTime.Now;
+
+            outputTextBox.AppendText($"Fibonacci(46) = {result1}\r\n");
+            double minutes = (endTime1 - startTime1).TotalMinutes;
+            outputTextBox.AppendText($"Calculation time = {minutes:F6} minutes\r\n");
+
+            outputTextBox.AppendText("Calculating Fibonacci(45)\r\n");
+            outputTextBox.Refresh();
+            DateTime startTime2 = DateTime.Now;
+            long result2 = Fibonacci(45);
+            DateTime endTime2 = DateTime.Now;
+
+            outputTextBox.AppendText($"Fibonacci(45) = {result2}\r\n");
+            minutes = (endTime2 - startTime2).TotalMinutes;
+            outputTextBox.AppendText($"Calculating time = {minutes:F6} minutes\r\n\r\n");
+
+            double totalMinutes = (endTime2 - startTime2).TotalMinutes;
+            outputTextBox.AppendText($"Total calculation time = {totalMinutes:F6} minutes\r\n");
+        }
+
+        // Calculates nth Fibonacci number recursively
+        public long Fibonacci(long n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
